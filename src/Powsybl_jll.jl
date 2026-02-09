d291a9e0afdcf43f98b9a71f7d60a55c60f8d40d
jlbuild <juliabuildbot@gmail.com>
2026-02-09 23:21:00 +0000

    Powsybl_jll build 0.3.0+0

--- src/Powsybl_jll.jl
+++ src/Powsybl_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Powsybl")
-JLLWrappers.@generate_main_file("Powsybl", UUID("b8c81e45-bfcc-5af0-87df-fd7619bc5515"))
+JLLWrappers.@generate_main_file("Powsybl", Base.UUID("b8c81e45-bfcc-5af0-87df-fd7619bc5515"))
 end  # module Powsybl_jll
